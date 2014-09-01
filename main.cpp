867fd845a79f8c069dc58e9bd3368ded6f46c53d
Turupawn <turupawn@userr>
2014-09-01 14:02:15 -0600

    primer commit

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include "NodoArbolBinario.h"
+#include "NodoArbolNArio.h"
+#include "Evaluador.h"
+#include <iostream>       // std::cin, std::cout
+#include <list>          // std::list
+using namespace std;
+
+int suma(NodoArbolBinario* raiz)
+{
+    return -1;
+}
+
+int suma(NodoArbolNArio* raiz)
+{
+    return -1;
+}
+
+int cantidadNodos(NodoArbolBinario* raiz)
+{
+    return -1;
+}
+
+int cantidadNodos(NodoArbolNArio* raiz)
+{
+    return -1;
+}
+
+int promedio(NodoArbolBinario* raiz)
+{
+    return -1;
+}
+
+int promedio(NodoArbolNArio* raiz)
+{
+    return -1;
+}
+
+int main ()
+{
+    //Funcion evaluadora
+    evaluar();
+    return 1;
+}
